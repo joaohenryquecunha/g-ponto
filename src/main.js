91431fb27952174090152788464efede89f4cff0
joao <henryque_joao17@hotmail.com>
2023-10-07 17:52:07 -0300

    Co-authored-by: Nathan Lima <NathanLima96@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import bootstrap from './bootstrap'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { ModalPlugin } from 'bootstrap-vue'
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
+Vue.use(ModalPlugin)
+Vue.use(bootstrap);
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cadastro/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Cadastro from '@/components/Cadastro.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+     Cadastro,
   }
 }
 </script>
